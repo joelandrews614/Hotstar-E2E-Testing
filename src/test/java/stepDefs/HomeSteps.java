454ce1352f6b849a381fad5dc5bf308a2d4717de
lultr <joelandrews614@gmail.com>
2024-06-03 17:07:00 +0530

    Added 1 Feature for Home Page

---
+++ src/test/java/stepDefs/HomeSteps.java
@@ -0,0 +1,21 @@
+package stepDefs;
+
+import io.cucumber.java.en.*;
+
+public class HomeSteps {
+	
+	@Given("user is on Homepage")
+	public void user_is_on_homepage() {
+
+
+	}
+
+	@Given("user can see the Navbar Icons")
+	public void user_can_see_the_navbar_icons() {
+
+
+	}
+
+
+	
+}
---
+++ target/test-classes/features/Home.feature
@@ -0,0 +1,6 @@
+Feature: Home Page Functionality of Hotstar
+
+  Scenario: Validating the Navbar Icons
+    Given user is on Homepage	
+    And user can see the Navbar Icons
+
